3216bbfc00f45e8182b62ac9915deb82fe326cbc
jubuyer <jahmed10@stuy.edu>
2022-08-27 22:41:40 -0400

    basics updated

--- bot.js
+++ bot.js
@@ -1,9 +1,18 @@
 // Follow documentation and add in your own twists to build your own simple bot!
 const eris = require('eris');
 
-//Client instance using bot token.
-//Replace change-token with your unique bot token!
-const bot = new eris.Client('change-token');
+var fs = require('fs');
+
+// Read's secret file token content (ignored in .gitignore file) into var 'my_token'
+const buffer = fs.readFileSync("secret.txt");
+
+const my_token = buffer.toString();
+
+// console.log(my_token);
+
+// Client instance using bot token.
+// Replace my-token with your unique bot token!
+const bot = new eris.Client(my_token);
 
 // Sends message to console when the bot is ready and connected
 bot.on('ready', () => {
@@ -20,7 +29,7 @@ bot.on('messageCreate', async (msg) => {
    // If the bot is mentioned, it will reply with 'I can hear you!'.
    if (botWasMentioned) {
        try {
-           await msg.channel.createMessage('Present');
+           await msg.channel.createMessage('I can hear you!');
        } catch (err) {
            // Sending messages can sometimes fail, this piece of
            // code helps us know if it did
